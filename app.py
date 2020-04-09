3fc6c4b73c686a7436afe36b31bf0738da0641d0
URVISH TALAVIYA <43312731+urvish667@users.noreply.github.com>
2020-04-10 00:45:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open('titanic_knn.pkl', 'rb'))
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods = ['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        pclass = int(request.form['pclass'])
+        sex = int(request.form['sex'])
+        embarked = int(request.form['embarked'])
+        title = int(request.form['title'])
+        deck = int(request.form['deck'])
+        family_size = int(request.form['family_size'])
+        age = int(request.form['age'])*pclass
+        fare = int(request.form['fare'])/family_size
+        final_feature = np.array([[pclass, sex, embarked, title, deck, family_size, age, fare]])
+        survived = model.predict(final_feature)
+        if survived == 1:
+            return render_template('home.html', result = "Passanger is lucky!! :)")
+        else:
+            return render_template('home.html', result = "Sorry for Passanger!! :(")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
