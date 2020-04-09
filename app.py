325ba174875a6a0ca89b25ec4f7451943cf98a2d
URVISH TALAVIYA <43312731+urvish667@users.noreply.github.com>
2020-04-10 01:11:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -10,23 +10,22 @@ model = pickle.load(open('titanic_knn.pkl', 'rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/predict', methods = ['GET', 'POST'])
+@app.route('/predict', methods = ['POST'])
 def predict():
-    if request.method == 'POST':
-        pclass = int(request.form['pclass'])
-        sex = int(request.form['sex'])
-        embarked = int(request.form['embarked'])
-        title = int(request.form['title'])
-        deck = int(request.form['deck'])
-        family_size = int(request.form['family_size'])
-        age = int(request.form['age'])*pclass
-        fare = int(request.form['fare'])/family_size
-        final_feature = np.array([[pclass, sex, embarked, title, deck, family_size, age, fare]])
-        survived = model.predict(final_feature)
-        if survived == 1:
-            return render_template('home.html', result = "Passanger is lucky!! :)")
-        else:
-            return render_template('home.html', result = "Sorry for Passanger!! :(")
+    pclass = int(request.form['pclass'])
+    sex = int(request.form['sex'])
+    embarked = int(request.form['embarked'])
+    title = int(request.form['title'])
+    deck = int(request.form['deck'])
+    family_size = int(request.form['family_size'])
+    age = int(request.form['age'])*pclass
+    fare = int(request.form['fare'])/family_size
+    final_feature = np.array([[pclass, sex, embarked, title, deck, family_size, age, fare]])
+    survived = model.predict(final_feature)
+    if survived == 1:
+        return render_template('home.html', result = "Passanger is lucky!! :)")
+    else:
+        return render_template('home.html', result = "Sorry for Passanger!! :(")
 
 
 if __name__ == "__main__":
